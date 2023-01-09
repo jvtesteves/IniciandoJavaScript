d4ce144d6662ecd7cfa32e679eed527d988231c3
Joao Victor Tavares Esteves <joaovtesteves2002@gmail.com>
2023-01-09 10:58:24 -0300

    Aula 05

---
+++ funcoes.js
@@ -0,0 +1,26 @@
+// let x = '';
+// console.log(x)
+// x = 'oi';
+// console.log(x)
+
+
+// reaproveitamento de codigo
+// declara a funcao 
+
+function imprimeTexto(texto) {
+    console.log(texto)
+}
+
+// executa a funcao
+
+imprimeTexto('jv');
+imprimeTexto('outro texto');
+
+function soma() {
+    const resultado = 2 + 2;
+    return resultado;
+}
+
+console.log(soma())
+
+imprimeTexto(soma())
---
+++ parametros.js
@@ -0,0 +1,14 @@
+// function soma() {
+//     return 2 + 2;
+// }
+
+// console.log(soma())
+
+// vamos passar agora operadores para a funcao soma
+
+function soma(a, b) {
+    return a + b; 
+}
+
+console.log(soma(1, 2))
+console.log(soma(10, 2))
